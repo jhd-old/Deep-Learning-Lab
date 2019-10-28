55c0ad77558483978b238c073ef95163cb9839e8
Wirth <fw95coqa@ads.tu-darmstadt.de>
2019-10-28 17:37:41 +0100

    get to know pytorch set up atom on pool pc

---
+++ reference code/ccn_with_pytorch/ch1-Introduction_to_PyTorch.py
@@ -0,0 +1,10 @@
+# creating tensors in pytorch
+# Import torch
+import torch
+# Create random tensor of size 3 by 3
+your_first_tensor = torch.rand(3, 3)
+# Calculate the shape of the tensor
+tensor_size = your_first_tensor.shape
+# Print the values of the tensor and its shape
+print(your_first_tensor)
+print(tensor_size)
