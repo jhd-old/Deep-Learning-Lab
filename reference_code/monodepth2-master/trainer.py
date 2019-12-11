49fcd3d93a52055108798dfb1697ab65acdca145
Korbinian <korbinian.kunst@ruecker-idstein.de>
2019-12-11 16:50:02 +0100

    minor change

--- reference_code/monodepth2-master/trainer.py
+++ reference_code/monodepth2-master/trainer.py
@@ -269,7 +269,7 @@ class Trainer:
                 img_sp = Image.fromarray(img_sp)
                 tensor[i] = trans1(img_sp)
 
-            features = self.models["encoder"](inputs["color_aug", 0, 0, tensor], secondInput = 0)
+            features = self.models["encoder"](inputs["color_aug", 0, 0], tensor)
             outputs = self.models["depth"](features)
         else:
             # Otherwise, we only feed the image with frame_id 0 through the depth encoder
