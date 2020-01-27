3ee28605e2ae0840833b25729338d5078c847467
Jan Coccejus <jan.coccejus@stud.tu-darmstadt.de>
2020-01-27 11:56:45 +0100

    write test code for normal2disp.py

--- surface_normals/TestCode_SurfaceNomal/main.py
+++ surface_normals/TestCode_SurfaceNomal/main.py
@@ -1,7 +1,10 @@
 import cv2
+# import torch
 import numpy as np
 import surface_normal_3
 import normal_to_depth
+import torch
+import normal2disp
 
 
 #depth =
@@ -11,12 +14,51 @@ print(cv2.__version__)
 #print(dlib.__version__)
 
 K = np.array([[70, 0, 50], [0, 70, 40], [0, 0, 1]])
+K_inv = np.linalg.pinv(K)
 
 
 if __name__ == "__main__":
-    #surface_normal_2.surface_normal('data/test_img.png')
+    # surface_normal_2.surface_normal('data/test_img.png')
     normal, d_im = surface_normal_3.surface_normal('data/test_img.png')
+
     depth = normal_to_depth.normal_to_depth(K, d_im, normal)
     disp = normal_to_depth.depth_to_disp(K, depth)
 
-    cv2.imwrite("normal2depth.png", disp*255)
+    cv2.imwrite("normal2depth.png", depth * 10255)
+    cv2.imwrite("normal2disp.png", disp * 10255)
+
+    ######################
+    # NEW CODE
+    ######################
+
+    # generate a tensor with the surface normals
+    normal_np = torch.from_numpy(normal)
+
+    # get normal_tensor into the shape for normal2depth. -> normal_tensor(batch_size, ch, h, w)
+
+    w, h = d_im.shape
+    # generate empty tensor
+    normal_tensor = torch.zeros((1, 3, w, h))
+
+    # unbind the input tensor to the three channels
+    normal_np = torch.unbind(normal_np, dim=2)
+
+    # rebuild the normal vector tensor to match the right shape
+    normal_tensor[0, 0, :, :] = normal_np[0]
+    normal_tensor[0, 1, :, :] = normal_np[1]
+    normal_tensor[0, 2, :, :] = normal_np[2]
+
+    # generate depth and disparity
+    new_depth = normal2disp.normal_to_depth(K_inv, normal_tensor)
+    new_disp = normal2disp.depth_to_disp(K, new_depth)
+
+    # remove not used dimensions
+    new_depth = torch.squeeze(new_depth)
+    new_disp = torch.squeeze(new_disp)
+    # turn back into numpy array
+    new_depth = new_depth.numpy()
+    new_disp = new_disp.numpy()
+
+    # save the image to a file
+    cv2.imwrite("normal2depth_fastFunction.png", new_depth * 10255)
+    cv2.imwrite("normal2disp_fastFunction.png", new_disp * 10255)
---
+++ surface_normals/TestCode_SurfaceNomal/normal2disp.py
@@ -0,0 +1,107 @@
+import numpy as np
+import torch
+from torch import nn
+
+
+def normal_to_depth(inv_K, normal):
+    """
+    Converts normal vectors to depth.
+
+    :param K_inv: inverted intrinsic matrix (Torch tensor)
+    :param normal: normals vector (Torch tensor)
+    :return: depth matrix
+    :rtype: Torch tensor
+    :todo: continue implementing and change to use this method in trainer.py
+    """
+
+    batch_size = normal.shape[0]
+    h = normal.shape[2]
+    w = normal.shape[3]
+
+    #inv_K = inv_K[0, 0:3, 0:3].cuda().float()
+    inv_K = torch.from_numpy(inv_K).float()
+    # init meshgrid for image height and width
+    meshgrid = np.meshgrid(range(w), range(h), indexing='xy')
+
+    # use numpy stack to create an ndarray out of the meshgrid
+    id_coords = np.stack(meshgrid, axis=0).astype(np.float32)
+
+    # convert numpy nd-array to a torch parameter (~tensor)
+    id_coords = nn.Parameter(torch.from_numpy(id_coords),
+                                  requires_grad=False)
+
+    # add one dimension to coordinates
+    pix_coords = torch.stack([id_coords[0].view(-1), id_coords[1].view(-1)], dim=0)
+
+    # unsqueeze of 1xn or nx1 tensor basically just changes there orientation
+    pix_coords = torch.unsqueeze(pix_coords, 0)
+
+    # repeat pixel coordinates for every batch size.
+    pix_coords = pix_coords.repeat(batch_size, 1, 1)
+
+    # init tensor with ones to be able to change the coordinates (2D) to homogenous coordinates (3D)
+    ones = nn.Parameter(torch.ones(batch_size, 1, h * w),
+                        requires_grad=False)
+
+    # concatinate ones to 2D points to get 3D homogenous form.
+    pix_coords = nn.Parameter(torch.cat([pix_coords, ones], 1),
+                                   requires_grad=False)
+
+    # matrix multiplication with 3x3 (k_inv) and
+    cam_points = torch.matmul(inv_K, pix_coords) # entspricht K^1*pixel
+
+    # convert numpy nd-array to a torch parameter (~tensor)
+    normal_vec = nn.Parameter(normal,
+                             requires_grad=False).float()
+
+    # get batch size
+    batch = normal_vec.shape[0]
+
+    # do some magic
+    normal_vectors = torch.stack([normal_vec[:, 0].view(batch, -1), normal_vec[:, 1].view(batch, -1),
+                                  normal_vec[:, 2].view(batch, -1)], dim=1)
+
+    # do dot product by multiplication of both matrices (x_r=x*x', y_r=y*y', z_r=z*z') and sum (d=x_r+y_r+z_r)
+    depth = (normal_vectors * cam_points).sum(1)
+
+    # depth = 1 / values
+    # matrix**(-1) should solve this
+    depth = depth.pow(-1)
+
+    # add dimension one to change (batch size, h*w) to (batch size, 1, h*w)
+    depth = torch.unsqueeze(depth, dim=1)
+
+    # unstack to retrieve one depth matrix per batch (batch size, 1, h, w)
+    depth = depth.view(batch_size, 1, h, w)
+
+    return depth
+
+
+def depth_to_disp(K, depth):
+    """
+    Converts depth to disparity.
+
+    :param K: intrinsic matrix
+    :param depth: depth matrix (batch size, 1, h, w)
+    :return:
+    """
+
+    # get batch size, height and width
+    batch, channel, h, w = depth.shape
+
+    # init an empty 4D tensor and put it onto gpu
+    disp = torch.empty(batch, 1, h, w)
+
+    # intrinsic matrix reduced to 3x3
+    # K = K[0, 0:3, 0:3].float()
+    K = K
+    # calculate focal length
+    focal_length = K[1, 1] * w
+
+    # distance between stereo cameras. Taken from kitti dataset.
+    baseline = 0.54
+
+    for n in range(0, batch):
+        disp[n, 0, :, :] = (baseline * focal_length) / (depth[n, 0, :, :] + 1e-8)
+
+    return disp
