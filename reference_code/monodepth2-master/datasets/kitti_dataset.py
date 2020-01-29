243b9764a23adc1a31f3b4dcbbe65b415c253833
jhd <helge.doersam@t-online.de>
2020-01-29 12:20:00 +0100

    hotfix

--- reference_code/monodepth2-master/datasets/kitti_dataset.py
+++ reference_code/monodepth2-master/datasets/kitti_dataset.py
@@ -133,6 +133,9 @@ class SuperpixelDataset(KITTIDataset):
         if do_flip:
             super_label = np.fliplr(super_label)
 
+        # add an empty dimension for channel
+        super_label = np.expand_dims(super_label, axis=2)
+
         # convert label to pillow image
         # since there is no 16bit support, we need to use 32bit:
         # mode I: (32-bit signed integer pixels)
