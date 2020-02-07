96676a08b1788ee0cf3956ac130f2bbf52c42561
jhd <helge.doersam@t-online.de>
2020-02-07 14:54:17 +0100

    Flatten normals

--- reference_code/monodepth2-master/trainer.py
+++ reference_code/monodepth2-master/trainer.py
@@ -544,7 +544,8 @@ class Trainer:
                 # calculate standard deviation for each area
                 # calculate first for each channel of current area, then sum for current area
                 try:
-                    normals_loss += np.sum(np.std(normals, axis=0))
+                    normals = normals.reshape((normals.shpae[0], normals.shape[1] * normals.shape[2]))
+                    normals_loss += np.sum(np.std(normals, axis=1))
                 except:
                     print("normal in np per batch:", normals_np.shape)
                     print("all normals per superpixel:", len(normals_per_superpixel))
