47156d2d2cd71ad3d3013d9bd8559080079ae53c
jhd <helge.doersam@t-online.de>
2020-02-07 15:17:55 +0100

    Next try

--- reference_code/monodepth2-master/trainer.py
+++ reference_code/monodepth2-master/trainer.py
@@ -538,15 +538,15 @@ class Trainer:
             normals_np = normals[b]
 
             # get all normals pixel values per superpixel area
-            normals_per_superpixel = [normals_np[idx-1] for idx in superpixel_list]
+            normals_per_superpixel = [normals_np[pixel] for pixel in superpixel_list]
 
             for normals in normals_per_superpixel:
                 print("single normal per superpixel shape: ", normals.shape)
                 # calculate standard deviation for each area
                 # calculate first for each channel of current area, then sum for current area
                 try:
-                    normals = normals.reshape((normals.shape[0], normals.shape[1] * normals.shape[2]))
-                    normals_loss += np.sum(np.std(normals, axis=(0, 1)))
+                    normals_flat = normals[:].reshape((normals.shape[0], normals.shape[1] * normals.shape[2]))
+                    normals_loss += np.sum(np.std(normals_flat, axis=(0, 1)))
                 except:
                     print("curent normal shape:", normals.shape)
                     print("all normals per superpixel:", len(normals_per_superpixel))
